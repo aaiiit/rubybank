884c5ffb6a41706eca63b033f0a6d67366ffc35d
Mestdagh Tom <mestdagh.tom@gmail.com>
2015-03-04 14:43:38 +0100

    An Account is now created after User creation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
   acts_as_authentic
+  has_one :account
+
+  after_create :create_account
+
+  protected
+  def create_account
+    puts "BUILDING ACCOUNT!!!!!!!!!!!!!"
+    self.account = Account.create
+    self.save
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "should be possible to create user with password" do
-    user = User.new(login: 'satoshi',password: 'nakomoto',password_confirmation: 'nakomoto')
-    expect(user.save).to eq(true)
-    expect(user.crypted_password).not_to eq(nil)
-  end
 
   it "should not be possible to create user without password" do
     user = User.new(login: 'satoshi')
     expect(user.save).to eq(false)
+  end
+
+  context "new valid user" do
+    before do
+      @user = User.new(login: 'satoshi',password: 'nakomoto',password_confirmation: 'nakomoto')
+      @user.save
+    end
+
+    it "should be possible to create user with password" do
+      expect(@user.persisted?).to eq(true)
+    end
+
+    it "should have created a crypted_password from the password" do
+      expect(@user.crypted_password).not_to eq(nil)
+    end
+
+    it "should have created an accompanying account for user" do
+      expect(@user.account).not_to eq(nil)
+      expect(@user).to eq(@user.account.user)
+    end
+
   end
 end
