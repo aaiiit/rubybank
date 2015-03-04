c318ab71e4bbc8ca101a4b09111811586952e2b6
Mestdagh Tom <mestdagh.tom@gmail.com>
2015-03-04 20:52:36 +0100

    Removed unnecessary views from transactions.

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,6 @@
 class TransactionsController < ApplicationController
-  before_action :set_transaction, only: [:show, :edit, :update, :destroy]
+  before_action :require_user
+  before_action :set_transaction, only: [:show]
 
   # GET /transactions
   # GET /transactions.json
@@ -17,10 +18,6 @@ class TransactionsController < ApplicationController
     @transaction = Transaction.new
   end
 
-  # GET /transactions/1/edit
-  def edit
-  end
-
   # POST /transactions
   # POST /transactions.json
   def create
@@ -34,30 +31,6 @@ class TransactionsController < ApplicationController
         format.html { render :new }
         format.json { render json: @transaction.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /transactions/1
-  # PATCH/PUT /transactions/1.json
-  def update
-    respond_to do |format|
-      if @transaction.update(transaction_params)
-        format.html { redirect_to @transaction, notice: 'Transaction was successfully updated.' }
-        format.json { render :show, status: :ok, location: @transaction }
-      else
-        format.html { render :edit }
-        format.json { render json: @transaction.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /transactions/1
-  # DELETE /transactions/1.json
-  def destroy
-    @transaction.destroy
-    respond_to do |format|
-      format.html { redirect_to transactions_url, notice: 'Transaction was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
